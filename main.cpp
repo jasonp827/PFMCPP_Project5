8f5aab5914fbc313f938bea67343df0eee0dba05
jasonp827 <jasonp827@gmail.com>
2020-06-26 20:36:45 +0000

    Finished Proj 5 Part 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <iostream>
+
 /*
 Project 5: Part 1 / 4
  video Chapter 2 - Part 12
@@ -35,24 +38,231 @@ Purpose:  This project continues developing Project3.
 /*
  copied UDT 1:
  */
+struct Oscillator
+{
+    Oscillator();
+    ~Oscillator();
+    int negativeTwelvePower;
+    int positiveTwelvePower;
+    float waveshape;
+    int outputType;
+    float fmAttenuator = 0.0f;
+    float phase = 0.0f;
+
+    bool outputWave(int negativePower, int positivePower);
+    void hardSync(std::vector<double> inputSignal);
+    void receivePitchCV(std::vector<int> sequence);
+    void resetPhase();
+};
+
+Oscillator::Oscillator() :
+negativeTwelvePower(40),
+positiveTwelvePower(30),
+waveshape(12.4f),
+outputType(2)
+{}
+
+Oscillator::~Oscillator()
+{
+    std::cout << "Oscillator\n";
+}
+bool Oscillator::outputWave(int negativePower, int positivePower)
+{
+    return (negativePower >= negativeTwelvePower && positivePower >= positiveTwelvePower);
+} 
+
+void Oscillator::resetPhase()
+{
+    phase = 0.0f;
+}
+
+void Oscillator::hardSync(std::vector<double> inputSignal)
+{
+   
+    for (unsigned long i = 0; i < inputSignal.size(); ++i)
+    {
+        if (inputSignal[i] == 0.0)
+        {
+            phase = 0.0f;
+        }
+    }
+}
+
+void Oscillator::receivePitchCV(std::vector<int> sequence)
+{
+    for (unsigned long i = 0; i < sequence.size(); i++)
+    {
+        std::cout << sequence[i] << ", ";
+    }
+    std::cout << "\n";
+}
 
 /*
  copied UDT 2:
  */
+struct Filter
+{
+    Filter();
+    ~Filter();
+    int filterType{1};
+    float fmAttenuator{0.0f};
+    float cutoff{1.0f};
+    float resonance{0.0f};
+    float gain{10.0f};
+
+    std::vector<float> movingAvrgFilter(std::vector<float> signal);
+    void changeResonance(float change);
+    void trackPitch(float cv);
+    void modFilter(float cv);
+
+};
+
+Filter::Filter()
+{}
+
+Filter::~Filter()
+{
+    std::cout << "Filter\n";
+}
+std::vector<float> Filter::movingAvrgFilter(std::vector<float> signal)
+{
+    for (unsigned long i = 0; i < signal.size(); ++i)
+    {
+        signal[i] = (signal[i] + signal[i-1] + signal[i-2] + signal[i-3])/4;
+    }// for loop iterates through signal and applies a moving average to the samples
+    return signal;
+}
+void Filter::changeResonance(float change)
+{
+    resonance += change;
+}
+
+void Filter::trackPitch(float cv)
+{
+    cutoff += cv;
+}
+
+void Filter::modFilter(float cv)
+{
+    //fmAttenuator works between values of 0.0f and 1.0f
+    cutoff += cv * fmAttenuator;
+    
+}
 
 /*
  copied UDT 3:
  */
+struct CVSequencer
+{
+    CVSequencer();
+    ~CVSequencer();
+    int cvChannels;
+    float outputLevel;
+    int steps = 16;
+    int trigPerStep = 1;
+    int scale;
+
+    void changeNumSteps(int newNumSteps);
+    void divideClock(int division);
+    std::vector<int> revArp(std::vector<int> noteOrder);
+};
+
+CVSequencer::CVSequencer() :
+cvChannels(8),
+outputLevel(0),
+scale(2)
+{}
+
+CVSequencer::~CVSequencer()
+{
+    std::cout << "CVSequencer\n";
+}
+
+
+void CVSequencer::changeNumSteps(int newNumSteps)
+{
+    steps = newNumSteps;
+}
+
+void CVSequencer::divideClock(int division)
+{
+    trigPerStep *= division;
+}
+
+std::vector<int> CVSequencer::revArp(std::vector<int> noteOrder)
+{
+    std::vector<int> revArp = {}; 
+    for (unsigned long i = noteOrder.size(); i-- != 0;)
+    {
+        revArp.push_back(noteOrder[i]);
+    }
+    return revArp;
+}
 
 /*
  new UDT 4:
  with 2 member functions
  */
+ struct Tone
+ {
+     Tone();
+     ~Tone();
+     Oscillator triangleCore;
+     Filter sem20;
+
+     void changeTone();
+ };
+
+ void Tone::changeTone()
+     {
+         sem20.filterType = (sem20.filterType++)%3;
+     }
+
+Tone::Tone()
+{}
+
+Tone::~Tone()
+{
+    changeTone();
+    std::cout << "Tone\n";
+}
 
 /*
  new UDT 5:
  with 2 member functions
  */
+ struct MelodyPlayer
+ {
+     MelodyPlayer();
+     ~MelodyPlayer();
+     Oscillator triangleCore;
+     CVSequencer voltageBlock;
+
+     void playArp();
+     void modWaveShape();
+ };
+
+MelodyPlayer::MelodyPlayer()
+{}
+
+MelodyPlayer::~MelodyPlayer()
+{
+    std::cout << "MelodyPlayer\n";
+}
+void MelodyPlayer::playArp()
+{
+    std::vector<int> newArp = voltageBlock.revArp({});
+    triangleCore.receivePitchCV(newArp);
+}
+
+void MelodyPlayer::modWaveShape()
+{
+    std::vector<int> newArp = voltageBlock.revArp({});
+    for (unsigned long i = 0; i < newArp.size(); i++)
+    {
+        triangleCore.waveshape += newArp[i];
+    }
+}
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
@@ -71,5 +281,21 @@ Purpose:  This project continues developing Project3.
 #include <iostream>
 int main()
 {
-    std::cout << "good to go!" << std::endl;
+    Oscillator sto, csL;
+    Filter sem20, ladderFilter;
+    CVSequencer voltageBlock;
+    sto.hardSync({1, 1, 1, 0, 1, 1, 1, 0});
+    std::cout << "hardSynced! Phase on this sample is reset: phase = " << sto.phase << "\n\n";
+    ladderFilter.movingAvrgFilter({2.32f, 23.2f, 86.3f, 43.1f, 92.6f, 123.0f, 62.5f, 53.9f, 24.2f});
+    std::vector<int> revArp = voltageBlock.revArp({1,2,3,4});
+    std::cout << "Reversed arp: ";
+    for(unsigned long i = 0; i < revArp.size(); ++i)
+    {
+        std::cout << revArp[i];
+    }
+    std::cout << "\n";
+    std::cout << "good to go!";
+
+    Tone tone;
+    MelodyPlayer melodyplayer;
 }
